4d9a260f9aafbf7d1a9bc48a2a8adf36e4c50981
Marcin <marcin92d@gmail.com>
2023-02-14 12:43:47 +0100

    Solution

--- src/main/java/pl/dolega/App.java
+++ src/main/java/pl/dolega/App.java
@@ -1,9 +1,38 @@
 package pl.dolega;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
 
     public static void main( String[] args ) {
 
+        String intsString;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter integers:");
+        while (true) {
+            intsString = scanner.nextLine();
+            if (intsString.matches("[-?0-9 ]+")) {
+                break;
+            } else {
+                System.out.println("Invalid input, try again:");
+            }
+        }
+        scanner.close();
+
+        List<Integer> ints = Arrays.stream(
+                intsString.split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+        ints.stream().sorted().forEach(i -> {
+            if ((i < 7) && ints.contains(13 - i)) {
+                System.out.println(i + " " + (13 - i));
+            }
+        });
     }
 }
+
